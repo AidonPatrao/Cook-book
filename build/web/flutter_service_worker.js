938294cbe796f5e1831857b25e64cb09c79e072d
AidonPatrao <aidonpatrao073@gmail.com>
2026-05-12 22:54:49 +0530

    Update Flutter web build

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -39,7 +39,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "6a20c3e453792b1b41741b02691e1676
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "85793fdf2b612f639588afc65fb6dbf4",
+"flutter_bootstrap.js": "2c550e22f021134d4d0496496f64a22a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
